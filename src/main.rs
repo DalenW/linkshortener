156f03854e0cb0b6f7a2f24754f962efab8407a1
Dalen Ward <dalen.ward@m6securities.com>
2021-02-23 12:25:21 -0700

    removed old test code

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,9 @@
-use std::{io::stdin, mem};
-
+use std::io::stdin;
 use linkshortener::{create_link, establish_connection};
 
 extern crate diesel;
 
-
-#[derive(Debug)]
-struct Person {
-    first_name: String,
-    last_name: String,
-    age: i8,
-}
-
-
-impl Person {
-    fn new(first: &str, last: &str, age: i8) -> Person {
-        let first_name: String = first.to_string();
-        let last_name: String = last.to_string();
-
-        mem::drop(first);
-        mem::drop(last);
-
-
-        assert!(age >= 0);
-        assert!(first_name != "");
-        assert!(last_name != "");
-
-        Person { first_name, last_name, age }
-    }
-
-    fn details(&self) -> String {
-        return format!("First Name: {}, Last Name {}, Age: {}", self.first_name, self.last_name, self.age);
-    }
-}
-
 fn main() {
-
-    /*
-    let p = Person::new("Dalen", "Ward", 22);
-
-    let mut people = Vec::<Person>::new();
-    people.push(Person::new("Amelia", "Ward", 19));
-    people.push(p);
-
-
-    println!("{:?}", people);
-
-    for p in people {
-        println!("{}", p.details());
-    }
-    */
 
     let connection = establish_connection();
 
