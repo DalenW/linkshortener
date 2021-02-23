9a536b6cfd37a205af8f05240ca3c9c9ed00695a
Dalen Ward <dalen.ward@m6securities.com>
2021-02-23 11:45:52 -0700

    models

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
+use std::mem;
+
+#[derive(Debug)]
 struct Person {
     first_name: String,
     last_name: String,
     age: i8,
 }
 
+
 impl Person {
     fn new(first: &str, last: &str, age: i8) -> Person {
         let first_name: String = first.to_string();
         let last_name: String = last.to_string();
+
+        mem::drop(first);
+        mem::drop(last);
 
 
         assert!(age >= 0);
@@ -18,11 +25,21 @@ impl Person {
     }
 
     fn details(&self) -> String {
-        format!("First Name: {}, Last Name {}, Age: {}", self.first_name, self.last_name, self.age)
+        return format!("First Name: {}, Last Name {}, Age: {}", self.first_name, self.last_name, self.age);
     }
 }
 
 fn main() {
     let p = Person::new("Dalen", "Ward", 22);
-    println!("{}", p.details());
+
+    let mut people = Vec::<Person>::new();
+    people.push(Person::new("Amelia", "Ward", 19));
+    people.push(p);
+
+
+    println!("{:?}", people);
+
+    for p in people {
+        println!("{}", p.details());
+    }
 }
--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,41 @@
+use std::iter;
+use std::mem;
+use rand::{Rng, thread_rng};
+use rand::distributions::Alphanumeric;
+
+
+
 #[derive(Queryable)]
 pub struct Link {
     pub id: i32,
     pub short: String,
     pub hyperlink: String,
 	pub enabled: bool,
+}
+
+impl Link {
+
+    fn new(link: &str) -> Link {
+        let id = 0;
+        let short: String = Link::generate_short();
+        let enabled: bool = true;
+        let hyperlink: String = link.to_string();
+
+        mem::drop(link);
+
+        assert!(hyperlink != "");
+
+        Link { id, short, hyperlink, enabled }
+    }
+    
+    fn generate_short() -> String {
+        // FROM: https://docs.rs/rand/0.8.3/rand/distributions/struct.Alphanumeric.html
+        let mut rng = thread_rng();
+        let chars: String = iter::repeat(())
+                .map(|()| rng.sample(Alphanumeric))
+                .map(char::from)
+                .take(7)
+                .collect();
+        return chars;
+    }
 }
