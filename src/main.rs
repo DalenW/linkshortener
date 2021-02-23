775620a2f9e055c7e7faf07d47abdb397bde385d
Dalen Ward <dalen.ward@m6securities.com>
2021-02-22 17:09:26 -0700

    Made an object

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+struct Person {
+    first_name: String,
+    last_name: String,
+    age: i8,
+}
+
+impl Person {
+    fn new(first: &str, last: &str, age: i8) -> Person {
+        let first_name: String = first.to_string();
+        let last_name: String = last.to_string();
+
+
+        assert!(age >= 0);
+        assert!(first_name != "");
+        assert!(last_name != "");
+
+        Person { first_name, last_name, age }
+    }
+
+    fn details(&self) -> String {
+        format!("First Name: {}, Last Name {}, Age: {}", self.first_name, self.last_name, self.age)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let p = Person::new("Dalen", "Ward", 22);
+    println!("{}", p.details());
 }
